f6d2c160f248ad2170d80204acbe81ae47cec090
Raymond <raymond@philippinedev.com>
2021-01-25 21:05:52 +0800

    unfinished dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root "homes#show"
+  constraints Clearance::Constraints::SignedIn.new do
+      root to: "dashboards#show"
+  end
+
+  root to: "homes#show"
+
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, only: [:create]
 
